d4a4ea0cbb1ea7861e040b6ba6dd4476ef63b0f9
nikolaienko19 <134286307+nikolaienko19@users.noreply.github.com>
2023-11-06 13:55:33 +0200

    first_commit

---
+++ app.js
@@ -0,0 +1,186 @@
+const cardArray = [
+    {
+        name: 'bachira_1',
+        img: 'images/image_1.png',
+    },
+
+    {
+        name: 'bachira_2',
+        img: 'images/image_2.png',
+    },
+
+    {
+        name: 'bachira_3',
+        img: 'images/image_3.png',
+    },
+
+    {
+        name: 'bachira_5',
+        img: 'images/image_5.png',
+    },
+
+    {
+        name: 'bachira_6',
+        img: 'images/image_6.png',
+    },
+
+    {
+        name: 'bachira_7',
+        img: 'images/image_7.png',
+    },
+
+    {
+        name: 'bachira_8',
+        img: 'images/image_8.png',
+    },
+
+    {
+        name: 'bachira_9',
+        img: 'images/image_9.png',
+    },
+
+    {
+        name: 'bachira_1',
+        img: 'images/image_1.png',
+    },
+
+    {
+        name: 'bachira_2',
+        img: 'images/image_2.png',
+    },
+
+    {
+        name: 'bachira_3',
+        img: 'images/image_3.png',
+    },
+
+    {
+        name: 'bachira_5',
+        img: 'images/image_5.png',
+    },
+
+    {
+        name: 'bachira_6',
+        img: 'images/image_6.png',
+    },
+
+    {
+        name: 'bachira_7',
+        img: 'images//image_7.png',
+    },
+
+    {
+        name: 'bachira_8',
+        img: 'images/image_8.png',
+    },
+
+    {
+        name: 'bachira_9',
+        img: 'images/image_9.png',
+    }
+]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+const resultDisplay = document.querySelector('#result')
+let buttonsWon = document.getElementById('buttons')
+let scoreText = document.querySelector('#score')
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/color_bg.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        gridDisplay.appendChild(card)
+
+    }
+}
+
+createBoard()
+
+function chechMatch() {
+    const cards = document.querySelectorAll('#grid img')
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
+    console.log(cards)
+    console.log('check for match!')
+    if (optionOneId == optionTwoId) {
+        cards[optionOneId].setAttribute('src', 'images/color_bg.png')
+        cards[optionTwoId].setAttribute('src', 'images/color_bg.png')
+    }
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert('Ти знайшла потрібного Бачіру!')
+        cards[optionOneId].setAttribute('src', 'images/grey_bg.png')
+        cards[optionTwoId].setAttribute('src', 'images/grey_bg.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/color_bg.png')
+        cards[optionTwoId].setAttribute('src', 'images/color_bg.png')
+        alert('Упс, не той Бачіра!')
+
+    }
+
+    resultDisplay.textContent = cardsWon.length
+    cardsChosen = []
+    cardsChosenIds = []
+
+    if (cardsWon.length === cardArray.length / 2) {
+        scoreText.textContent = 'Вітаю, ти знайшла всіх Бачір!'
+        buttonsWon.style.display = "flex"
+    }
+}
+
+function flipCard() {
+    console.log(cardArray)
+    const cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    console.log('clicked', cardId)
+    console.log(cardsChosen)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length == 2) {
+        setTimeout(chechMatch, 500)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ appfinal.js
@@ -0,0 +1,207 @@
+const cardArray = [
+    {
+        name: 'kaiser_1',
+        img: 'images/image_111.png',
+    },
+
+    {
+        name: 'kaiser_2',
+        img: 'images/image_222.png',
+    },
+
+    {
+        name: 'kaiser_3',
+        img: 'images/image_333.png',
+    },
+
+    {
+        name: 'kaiser_4',
+        img: 'images/image_444.png',
+    },
+
+    {
+        name: 'kaiser_5',
+        img: 'images/image_555.png',
+    },
+
+    {
+        name: 'kaiser_6',
+        img: 'images/image_666.png',
+    },
+
+    {
+        name: 'kaiser_7',
+        img: 'images/image_777.png',
+    },
+
+    {
+        name: 'kaiser_8',
+        img: 'images/image_888.png',
+    },
+
+    {
+        name: 'kaiser_9',
+        img: 'images/image_999.png',
+    },
+
+    {
+        name: 'kaiser_0',
+        img: 'images/image_000.png',
+    },
+
+    {
+        name: 'kaiser_1',
+        img: 'images/image_111.png',
+    },
+
+    {
+        name: 'kaiser_2',
+        img: 'images/image_222.png',
+    },
+
+    {
+        name: 'kaiser_3',
+        img: 'images/image_333.png',
+    },
+
+    {
+        name: 'kaiser_4',
+        img: 'images/image_444.png',
+    },
+
+    {
+        name: 'kaiser_5',
+        img: 'images/image_555.png',
+    },
+
+    {
+        name: 'kaiser_6',
+        img: 'images/image_666.png',
+    },
+
+    {
+        name: 'kaiser_7',
+        img: 'images/image_777.png',
+    },
+
+    {
+        name: 'kaiser_8',
+        img: 'images/image_888.png',
+    },
+
+    {
+        name: 'kaiser_9',
+        img: 'images/image_999.png',
+    },
+
+    {
+        name: 'kaiser_0',
+        img: 'images/image_000.png',
+    }
+
+]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+const resultDisplay = document.querySelector('#result')
+let buttonsWon = document.getElementById('buttons')
+let scoreText = document.querySelector('#score')
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/sparkles_bg.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        gridDisplay.appendChild(card)
+
+    }
+}
+
+createBoard()
+
+function chechMatch() {
+    const cards = document.querySelectorAll('#grid img')
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
+    console.log(cards)
+    console.log('check for match!')
+    if (optionOneId == optionTwoId) {
+        cards[optionOneId].setAttribute('src', 'images/sparkles_bg.png')
+        cards[optionTwoId].setAttribute('src', 'images/sparkles_bg.png')
+    }
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert('Ти знайшла потрібного Кайзера!')
+        cards[optionOneId].setAttribute('src', 'images/grey_bg_000.png')
+        cards[optionTwoId].setAttribute('src', 'images/grey_bg_000.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/sparkles_bg.png')
+        cards[optionTwoId].setAttribute('src', 'images/sparkles_bg.png')
+        alert('Упс, не той Кайзер!')
+
+    }
+
+    resultDisplay.textContent = cardsWon.length
+    cardsChosen = []
+    cardsChosenIds = []
+
+    if (cardsWon.length === cardArray.length / 2) {
+        scoreText.textContent = 'Вітаю, ти знайшла всіх Голубих диктаторів!'
+        buttonsWon.style.display = "flex"
+    }
+}
+
+function flipCard() {
+    console.log(cardArray)
+    const cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    console.log('clicked', cardId)
+    console.log(cardsChosen)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length == 2) {
+        setTimeout(chechMatch, 500)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
